cda053ecc1c33d24edde0d0dfd3a53b9dc4279b7
Dimosthenis Kalaitzis <dkalaitzis@ttswireless.com>
2019-07-03 16:15:49 +0300

    Added mysql db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { StatusMonitorModule } from 'nest-status-monitor';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+            useFactory: (config: ConfigService) => ({
+                type: 'mysql',
+                host: config.get('DOCKERIZED') ? 'ms_db' : 'localhost',
+                port: config.get('DOCKERIZED') ? 3306 : 33061,
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_DATABASE'),
+                entities: [__dirname + '/**/*.dbentity{.ts,.js}'],
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
         StatusMonitorModule.setUp({
             pageTitle: 'Nest Starter Project',
             port: 3000,
